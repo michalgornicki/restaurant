82136127102fbbdc7f8e911cbf140fe65a1d1601
michalgornicki <63716467+michalgornicki@users.noreply.github.com>
2021-01-28 14:42:04 +0100

    First upload

---
+++ myScript.js
@@ -0,0 +1,136 @@
+window.onload = function() {
+  document.getElementsByClassName("content")[0].style.transition="2s";
+  document.getElementsByClassName("content")[0].style.filter="brightness(1)";
+  document.getElementById("navbar").style.transition="2s";
+  document.getElementById("navbar").style.filter="opacity(1)";
+}
+
+function openMenu() {
+  document.getElementById("mobile-sliding-menu").style.filter="opacity(0.8)";
+  document.getElementById("mobile-sliding-menu").style.visibility="visible";
+  document.getElementById("mobile-sliding-menu").style.top="175px";
+  document.getElementById("burger-button").style.filter="opacity(0.0)";
+  document.getElementById("close-button").style.filter="opacity(1)";
+}
+
+function closeMenu() {
+  document.getElementById("mobile-sliding-menu").style.filter="opacity(0.8)";
+  document.getElementById("mobile-sliding-menu").style.visibility="visible";
+  document.getElementById("mobile-sliding-menu").style.top="-1250px";
+  document.getElementById("burger-button").style.filter="opacity(1)";
+  document.getElementById("close-button").style.filter="opacity(0)";
+}
+
+
+function fadePhoto1() {
+  if (document.body.scrollTop > 1850 || document.documentElement.scrollTop > 1850) {
+  document.getElementById("navbar").style.filter="opacity(0)";
+  document.getElementById("navbar").style.visibility="hidden";
+}
+  else {
+  document.getElementById("navbar").style.filter="opacity(1)";
+  document.getElementById("navbar").style.visibility="visible";
+}
+}
+
+window.onscroll = function() {
+  fadePhoto1()
+}
+
+document.getElementById("container1").style.backgroundColor="rgb(255, 245, 235)";
+
+
+function nightMode(){
+  if (document.getElementById("container1").style.backgroundColor==="rgb(255, 245, 235)") {
+  const darkMode =
+  document.getElementById("nightday-mode").style.filter="saturate(1) opacity(1)";
+  document.getElementById("image1").classList.remove("backgroundFade");
+  document.getElementById("image1").classList.remove("backgroundFadeOff");
+  document.getElementById("image1").classList.add("backgroundFade");
+  document.getElementById("navbar").style.boxShadow="0px 0px 50px 0px rgb(167, 122, 76)";
+  setTimeout(function(){
+    document.getElementById("image1").classList.add("backgroundFadeOff");
+    document.getElementsByClassName("container")[0].style.backgroundColor="rgb(110, 95, 80)";
+    document.getElementsByClassName("container")[1].style.backgroundColor="rgb(110, 95, 80)";
+    document.getElementsByClassName("container")[2].style.backgroundColor="rgb(110, 95, 80)";
+    document.getElementsByClassName("container")[3].style.backgroundColor="rgb(110, 95, 80)";
+    document.getElementsByClassName("footer")[0].style.color="rgb(255, 255, 255)";
+    document.getElementsByClassName("footer")[1].style.color="rgb(255, 255, 255)";
+    document.getElementsByClassName("header")[0].style.color="rgb(252, 223, 193)";
+    document.getElementsByClassName("header")[1].style.color="rgb(252, 223, 193)";
+    document.getElementsByClassName("alt-header")[0].style.color="rgb(252, 223, 193)";
+    document.getElementsByClassName("alt-header")[0].style.borderBottom="2px inset rgb(252, 223, 193)";
+    document.getElementById("header-contact").style.color="rgb(252, 223, 193)";
+    document.getElementById("header-contact").style.borderBottom="2px inset rgb(252, 223, 193)";
+    document.getElementById("meals").style.color="rgb(255, 255, 255)";
+    document.getElementById("list-beginning").style.color="rgb(255, 255, 255)";
+    document.getElementById("contact-list").style.color="rgb(255, 255, 255)";
+    document.getElementById("map-subtitle").style.color="rgb(255, 255, 255)";
+    document.getElementById("photo1").style.outline="inset 30px rgb(154, 133, 110)";
+    document.getElementById("photo2").style.outline="inset 30px rgb(154, 133, 110)";
+    document.getElementById("photo3").style.outline="inset 30px rgb(154, 133, 110)";
+    document.getElementById("photo4").style.outline="inset 30px rgb(154, 133, 110)";
+    document.getElementById("map").style.border="ridge 30px rgb(154, 133, 110)";
+    document.getElementById("image1").style.backgroundImage="url(background1night.jpg)";
+    document.getElementById("photo1small").style.outline="inset 30px rgb(154, 133, 110)";
+    document.getElementById("photo2small").style.outline="inset 30px rgb(154, 133, 110)";
+    document.getElementById("photo3small").style.outline="inset 30px rgb(154, 133, 110)";
+    document.getElementsByClassName("reservation")[0].style.color="rgb(255, 255, 255)";
+    document.getElementById("map-subtitle").style.color="rgb(255, 255, 255)";
+    document.getElementsByClassName("open-hours")[0].style.borderBottom="2px inset rgb(252, 223, 193)";
+    document.getElementsByClassName("brown-font")[0].style.color="rgb(252, 223, 193)";
+    document.getElementsByClassName("brown-font")[1].style.color="rgb(252, 223, 193)";
+    document.getElementsByClassName("brown-font")[2].style.color="rgb(252, 223, 193)";
+    document.getElementsByClassName("brown-font")[3].style.color="rgb(252, 223, 193)";
+  }, 1500);
+
+  }
+  else {
+  const lightMode =
+  document.getElementById("nightday-mode").style.filter="saturate(0) opacity(0.5)";
+  document.getElementById("image1").classList.remove("backgroundFade");
+  document.getElementById("image1").classList.remove("backgroundFadeOff");
+  document.getElementById("image1").classList.add("backgroundFade");
+  setTimeout(function(){
+    document.getElementById("image1").classList.add("backgroundFadeOff");
+    document.getElementsByClassName("container")[0].style.backgroundColor="rgb(255, 245, 235)";
+    document.getElementsByClassName("container")[1].style.backgroundColor="rgb(255, 245, 235)";
+    document.getElementsByClassName("container")[2].style.backgroundColor="rgb(255, 245, 235)";
+    document.getElementsByClassName("container")[3].style.backgroundColor="rgb(255, 245, 235)";
+    document.getElementsByClassName("footer")[0].style.color="rgb(74, 74, 74)";
+    document.getElementsByClassName("footer")[1].style.color="rgb(74, 74, 74)";
+    document.getElementsByClassName("header")[0].style.color="rgb(158, 0, 0)";
+    document.getElementsByClassName("header")[1].style.color="rgb(158, 0, 0)";
+    document.getElementsByClassName("alt-header")[0].style.color="rgb(158, 0, 0)";
+    document.getElementsByClassName("alt-header")[0].style.borderBottom="2px inset rgb(158, 0, 0)";
+    document.getElementById("header-contact").style.color="rgb(158, 0, 0)";
+    document.getElementById("header-contact").style.borderBottom="2px inset rgb(158, 0, 0)";
+    document.getElementById("meals").style.color="rgb(74, 74, 74)";
+    document.getElementById("list-beginning").style.color="rgb(74, 74, 74)";
+    document.getElementById("contact-list").style.color="rgb(74, 74, 74)";
+    document.getElementById("map-subtitle").style.color="rgb(74, 74, 74)";
+    document.getElementById("photo1").style.outline="inset 30px rgb(252, 223, 193)";
+    document.getElementById("photo2").style.outline="inset 30px rgb(252, 223, 193)";
+    document.getElementById("photo3").style.outline="inset 30px rgb(252, 223, 193)";
+    document.getElementById("photo4").style.outline="inset 30px rgb(252, 223, 193)";
+    document.getElementById("map").style.border="ridge 30px rgb(252, 223, 193)";
+    document.getElementById("image1").style.backgroundImage="url(1.jpg)";
+    document.getElementById("photo1small").style.outline="inset 30px rgb(252, 223, 193)";
+    document.getElementById("photo2small").style.outline="inset 30px rgb(252, 223, 193)";
+    document.getElementById("photo3small").style.outline="inset 30px rgb(252, 223, 193)";
+    document.getElementsByClassName("reservation")[0].style.color="rgb(255, 245, 235)";
+    document.getElementById("map-subtitle").style.color="rgb(255, 245, 235)";
+    document.getElementsByClassName("open-hours")[0].style.borderBottom="2px inset rgb(189, 85, 0)";
+    document.getElementsByClassName("brown-font")[0].style.color="rgb(189, 85, 0)";
+    document.getElementsByClassName("brown-font")[1].style.color="rgb(189, 85, 0)";
+    document.getElementsByClassName("brown-font")[2].style.color="rgb(189, 85, 0)";
+    document.getElementsByClassName("brown-font")[3].style.color="rgb(189, 85, 0)";
+  }, 1500);
+  }
+}
+
+
+style.backgroundColor="rgb(255, 245, 235)";
+style.border="inset rgb(252, 223, 193) 30px";
+("footer")[0].style.color="rgb(74, 74, 74)";
+("header")[0].style.color="rgb(158, 0, 0)";
